9fa8f703ba3c21cac69c33fcdef9e6de6c00da0e
EC2 Default User <ec2-user@ip-172-31-83-195.ec2.internal>
2020-09-18 17:01:45 +0000

    Experiment with JSON

--- spoonacular_api.py
+++ spoonacular_api.py
@@ -2,13 +2,22 @@ import requests
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import json
 
 dotenv_path = join(dirname(__file__), 'spoonacular.env')
 load_dotenv(dotenv_path)
 
 spoonacular_key = os.environ['SPOONACULAR_KEY']
-url = "https://api.spoonacular.com/recipes/search?diet=gluten%20free&apiKey={}".format(spoonacular_key)
+url = "https://api.spoonacular.com/recipes/search?apiKey={}".format(spoonacular_key)
 
 response = requests.get(url)
 json_body = response.json()
-print(type(json_body))
+#print((json_body))
+# print(json_body["results"][0])
+# print(json_body["results"][0]["title"])
+
+print(json.dumps(json_body["results"][0], indent=2))
+# print(json_body["results"][0])
+print(json_body["results"][0]["title"])
+
+
